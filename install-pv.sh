9a2e188c11350b1b414a6b88dc0e7f4c1e552894
Bill Plein <260078+bplein@users.noreply.github.com>
2023-12-13 09:20:08 -0600

    Cleanup
    General cleanup

--- install-pv.sh
+++ install-pv.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 packagesNeeded='pv'
+echo ""
+echo "About to install the pipe-viewer (pv) package. You may be asked for your password in order to run sudo"
+echo ""
 if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded
 elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $packagesNeeded
 elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
--- sqlstuff.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE vistor_log (firstname text, favoritecolor text, created_at timestamp);
-
-INSERT INTO visitor_log (firstname, favoritecolor, created_at) VALUES ($(firstname), $(favoritecolor), $(created_at));
